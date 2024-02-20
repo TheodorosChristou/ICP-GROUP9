d72853367853311d2f12747a84b14fb314a1e7d9
George Klitou <5kleig36@solent.ac.uk>
2024-02-20 18:50:18 +0000

    added map

---
+++ src/components/DynamicMap.tsx
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import L, { LatLngTuple } from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import markIcon from '../../public/img/mark.ico';
+import userIcon from '../../public/img/user.ico';
+
+
+
+export default function DynamicMap({ mapData }) {
+  const [userLocation, setUserLocation] = useState<LatLngTuple | null>(null);
+  const [isDefaultLocation, setIsDefaultLocation] = useState<boolean>(false);
+
+
+  // Custom Icon for other markers
+  const customIcon = new L.Icon({
+    iconUrl: markIcon.src,
+    iconSize: [78, 64],
+  });
+
+  // Custom Icon for the user's marker
+  const newUserIcon = new L.Icon({
+    iconUrl: userIcon.src,
+    iconSize: [32, 32],
+  });
+
+  useEffect(() => {
+    const handlePermission = async () => {
+      // Check if geolocation is supported
+      if ('geolocation' in navigator) {
+        // Ask for permission
+        const permissionGranted = window.confirm(("Do you want to enable location services?"));
+        
+        if (permissionGranted) {
+          try {
+            const position = await new Promise<GeolocationPosition>((resolve, reject) => {
+              navigator.geolocation.getCurrentPosition(resolve, reject);
+            });
+
+            const { latitude, longitude } = position.coords;
+            setUserLocation([latitude, longitude]);
+          } catch (error: any) { // Explicitly type error
+            console.error(("Error getting user location:") + ' ' + error.message);
+            setUserLocation([51.5072, 0.1276]); // Use default coordinates if there is an error
+            setIsDefaultLocation(true);
+          }
+        } else {
+          setUserLocation([51.5072, 0.1276]); // Use default coordinates if permission is denied
+          setIsDefaultLocation(true);
+        }
+      } else {
+        console.error(("Geolocation is not supported by your browser"));
+        setUserLocation([51.5072, 0.1276]); // Use default coordinates if geolocation is not supported
+        setIsDefaultLocation(true);
+      }
+    };
+
+    handlePermission();
+  }, []);
+
+  const SetViewOnMap = () => {
+    const map = useMap();
+    if (userLocation) {
+      map.setView(userLocation, 16);
+    }
+    return null;
+  };
+
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center">
+      <div className="w-screen h-screen">
+        {/* Map Rendering */}
+        <MapContainer
+          style={{ width: '100%', height: '100%' }}
+          center={userLocation || [51.5072, 0.1276]}
+          zoom={16}
+          scrollWheelZoom={true}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {/* Rendering a marker for each datapoint in the mapData array */}
+          {mapData.map((dataPoint) => (
+            <Marker position={[dataPoint.Lat, dataPoint.Lon]} icon={customIcon} key={dataPoint._id}>
+              {/* Popup for each data point */}
+              <Popup>
+                <div>{dataPoint.City} - {dataPoint.Description}</div>
+              </Popup>
+            </Marker>
+          ))}
+          {/* Rendering a marker for the user with the new user icon only when permission is granted and not at the hardcoded coordinates */}
+          {userLocation && (
+            <Marker position={userLocation} icon={newUserIcon}>
+              {/* Popup for enabling location access */}
+              <Popup className="text-white p-4 rounded-md">
+                <div>
+                  <h3>
+                    Latitude: {userLocation[0].toFixed(6)}, Longitude: {userLocation[1].toFixed(6)}
+                  </h3>
+                </div>
+              </Popup>
+            </Marker>
+          )}
+          <SetViewOnMap />
+        </MapContainer>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/map.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { GetServerSideProps } from 'next';
+import dbConnect from '../../lib/dbConnect';
+import ObservationModel, { ObservationInterface } from '../../models/Observation'; // Import ObservationModel and ObservationInterface
+import dynamic from 'next/dynamic';
+
+// Dynamic map, using Next.js dynamic for lazy loading for performance
+const DynamicMap = dynamic(() => import("../components/DynamicMap"), {
+  ssr: false,
+});
+// This helps with shaping the data that the DynamicMap will receive  from the mapData prop
+interface MapPageProps {
+  mapData: ObservationInterface[];
+}
+
+//Accepts mapData prop as an array of datapoints
+export default function Dmap({ mapData }: MapPageProps) {
+  return <DynamicMap mapData={mapData} />;
+}
+
+//Next.js function, ensures that the connection is there before fetching data
+export const getServerSideProps: GetServerSideProps<MapPageProps> = async () => {
+  await dbConnect(); // Connect to the MongoDB database
+
+  // Fetch data from MongoDB and convert them to javascript objects
+  const results = await ObservationModel.find({}).lean();
+  //adds a stringified _id
+  const map = results.map((doc) => ({ ...doc, _id: doc._id.toString() }));
+//returns a prop that contains data to be transfered to the DynamicMap
+  return { props: { mapData: map } };
+};
