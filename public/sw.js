2343ba8bb1d05c4937fe2e679265312c50536b86
Jakub Sepiolo <5sepij35@solent.ac.uk>
2024-03-02 21:22:27 +0000

    Working offline cache

--- public/sw.js
+++ public/sw.js
@@ -5,3 +5,25 @@ self.addEventListener("install", event => {
     console.log("Service worker activated");
  });
  
+ const cacheName = 'v1'
+
+ const cacheClone = async (e) => {
+   const res = await fetch(e.request);
+   const resClone = res.clone();
+ 
+   const cache = await caches.open(cacheName);
+   await cache.put(e.request, resClone);
+   return res;
+ };
+ 
+ const fetchEvent = () => {
+   self.addEventListener('fetch', (e) => {
+     e.respondWith(
+       cacheClone(e)
+         .catch(() => caches.match(e.request))
+         .then((res) => res)
+     );
+   });
+ };
+ 
+ fetchEvent();
